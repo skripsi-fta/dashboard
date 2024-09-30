114c4bede3e44d772ac295f6e48c437f20ec646c
Fabian Habil <fabianhabilramdhan@gmail.com>
2024-09-30 22:22:48 +0700

    wip

---
+++ src/presentation/pages/dashboard/management/jadwal/reguler/Component.tsx
@@ -0,0 +1,11 @@
+import DashboardContent from '@/presentation/layout/dashboard/content';
+
+const ScheduleRegulerManagementComponent = () => {
+    return (
+        <>
+            <DashboardContent>tes reguler</DashboardContent>
+        </>
+    );
+};
+
+export default ScheduleRegulerManagementComponent;
